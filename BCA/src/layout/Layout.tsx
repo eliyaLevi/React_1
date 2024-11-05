1a3aed5f5963b27561de7819bda50f1dce9c49db
eliyaLevi <el0547135208@gmail.com>
2024-11-05 10:57:57 +0200

    first commit

---
+++ BCA/src/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from './Header/Header'
+import { Main } from './Main/Main'
+import {Footer} from "./Footer/Footer"
+
+export const Layout = () => {
+  return (
+    <>
+     <Header/>
+    <Main/>
+    <Footer/>
+    </>
+   
+  )
+}
